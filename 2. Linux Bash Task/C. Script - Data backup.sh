69dd85ae4e3254595dcff131e76167a21d07e9bb
Ivan2navI <ivan.ivashka@i.ua>
2022-10-29 16:19:59 +0300

    C. Script - Update

--- 2. Linux Bash Task/C. Script - Data backup.sh
+++ 2. Linux Bash Task/C. Script - Data backup.sh
@@ -6,53 +6,82 @@ clear
 # Bash has a special type of redirect called Here document (heredoc) that allows you to do that
 
 cat << EOF
-# -----------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 #						C. Script - Data backup.sh
-# -----------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 # C. Create a data backup script that takes the following data as parameters: 
 #     1. Path to the syncing  directory. 
 #     2. The path to the directory where the copies of the files will be stored. 
 #
-# In case of adding new or deleting old files, the script must add a corresponding entry to the log file 
-# indicating the time, type of operation and file name. [The command to run the script must be added to 
-# crontab with a run frequency of one minute] 
-# -----------------------------------------------------------------------------------------------------------
+# In case of adding new or deleting old files, the script must add a corresponding 
+# entry to the log file indicating the time, type of operation and file name. 
+#
+# [The command to run the script must be added to crontab 
+# with a run frequency of one minute] 
+# ---------------------------------------------------------------------------------
 EOF
 echo
 
 
 # ==================================== FUNCTIONS ====================================
 
+# DATA info
+backupdate=$(date +%Y%m%d%H%M%S)
+echo "===================================================================="
+echo "Local TIME $backupdate"
+echo "===================================================================="
+echo
+
+
+Backup_FileName=Backup_Setiings_for_C_Task.txt
+FILE=./$Backup_FileName
+Line1_Backup_Settings="Path to source:"
+Line2_Backup_Settings="Path to destination:"
+
+func_Backup_FileName_Create() {
+  echo "===================================================================="
+  echo "$Backup_FileName creating ... Done!"
+  echo "Backup Setiings Create => $(date +%Y.%m.%d\ %Hh%Mm%Ss)" >> $Backup_FileName
+  echo "$Line1_Backup_Settings $Path_From" >> $Backup_FileName
+  echo "$Line2_Backup_Settings $Path2Sync">> $Backup_FileName
+  chmod a+rw $FILE
+  echo "===================================================================="
+  echo
+}
+
+
+func_Path_2Sync_Directories_Create() {
+  echo "===================================================================="
+  echo -n "Path for Source of Syncing: "
+  read Path_From
+  
+  until [ -d $Path_From ]
+  do
+    echo "$Path_From doesn't exist. Please, create this directory or enter correct path:"
+    read Path_From
+  done
+  echo "$Line1_Backup_Settings => $Path_From"
+  
+  echo "--------------------------------------------------------------------"  
+  
+  echo -n "Path to Destination: "
+  read Path2Sync
+  
+  until [ -d $Path2Sync ]
+  do
+    echo "$Path2Sync doesn't exist. Please, create this directory or enter correct path:"
+    read Path2Sync
+  done
+  echo "$Line2_Backup_Settings => $Path2Sync" 
+  
+  echo "===================================================================="
+  echo
+  #exit
+}
 
 # ==================================== ========= ====================================
 
 # ====================================   MAIN    ====================================
-
-# DATA info
-backupdate=$(date +%Y%m%d%H%M%S)
-echo $backupdate
-echo "===================================================================="
-echo
-
-
-# ==================================== ========= ====================================
-
-
-echo -n "Path for Source of Syncing: "
-read Path_From
-echo "Path to the syncing directory => $Path_From"
-
-echo -n "Path to the Destination: "
-read Path2Sync
-echo "The path to the directory where the copies of the files will be stored => $Path2Sync"
-echo "===================================================================="
-echo
-
-# ==================================== ========= ====================================
-
-Backup_FileName=Data_backup_script_Control.txt
-
-FILE=./$Backup_FileName
 
 if [ -w $FILE ]; then
   # При проверке существования файла наиболее часто используются операторы FILE -e и -f . 
@@ -61,15 +90,18 @@ if [ -w $FILE ]; then
   # -e file    Проверяет, существует ли файл.
   # -f file    Проверяет, существует ли файл, и является ли он файлом.
   # -w file    Проверяет, существует ли файл, и доступен ли он для записи.
-
-  
+  echo "===================================================================="  
   echo "$Backup_FileName exists, so Data backup was configureted."
-  #echo
+  echo "===================================================================="
 else 
-  echo "$Backup_FileName creating ... Done!"
-  echo "ARGUMENTS=> $backupdate" >> $Backup_FileName
-  echo "Path for Source of Syncing: $Path_From" >> $Backup_FileName
-  echo "Path to the Destination: $Path2Sync">> $Backup_FileName
-  chmod +x $FILE
-  echo  
+  func_Path_2Sync_Directories_Create
+  func_Backup_FileName_Create
+  
 fi
+echo
+
+# ==================================== ========= ====================================
+
+
+
+# ==================================== ========= ====================================
