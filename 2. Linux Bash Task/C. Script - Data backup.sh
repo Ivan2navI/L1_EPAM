a94a0c161e2f9629279b955a881cd195b9ccab30
Ivan2navI <ivan.ivashka@i.ua>
2022-10-30 01:14:17 +0300

    C. Script - Update

--- 2. Linux Bash Task/C. Script - Data backup.sh
+++ 2. Linux Bash Task/C. Script - Data backup.sh
@@ -36,25 +36,47 @@ echo
 Path_Config=".config_Script_Data_backup"
 Backup_FileName=Backup_Setiings_for_C_Task.txt
 LOGFILE=logs_sync_path_from2to.log
-
+LOGDATA=$(date +%Y%m%d_%H%M%S)
+for_log_file_info=$Path_Config/$LOGFILE
+  
 FILE=./$Path_Config/$Backup_FileName
 
 Line1_Backup_Settings="Path to source:"
 Line2_Backup_Settings="Path to destination:"
 
-func_Backup_FileName_Create() {
+func_Backup_and_LOG_File_Create() {
   echo "===================================================================="
   mkdir $Path_Config
   chmod a+rwx $Path_Config
   cd $Path_Config
-    
-  echo "$Backup_FileName creating ... Done!"
+
   echo "Backup Setiings Create => $(date +%Y.%m.%d\ %Hh%Mm%Ss)" >> $Backup_FileName
+  echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" >> $Backup_FileName
   echo "$Line1_Backup_Settings $Path_From" >> $Backup_FileName
   echo "$Line2_Backup_Settings $Path2Sync">> $Backup_FileName
-  echo "LOGFILE create at: $(pwd)/$LOGFILE">> $Backup_FileName
-  echo "Backup Setiings Done => $(date +%Y.%m.%d\ %Hh%Mm%Ss)" >> $Backup_FileName
   chmod a+rwx $Backup_FileName
+  
+  echo "$(date +%Y%m%d_%H%M%S) == LOGFILE create at: $(pwd)/$LOGFILE">> $LOGFILE
+  chmod a+rwx $LOGFILE
+  echo "$(pwd)/$LOGFILE creating ... Done!"  
+  
+  touch list_files_in_FROM.txt
+  chmod a+rwx list_files_in_FROM.txt
+  
+  echo "$LOGDATA == touch list_files_in_FROM.txt">> $LOGFILE
+  
+  touch list_files_in_TO.txt
+  chmod a+rwx list_files_in_TO.txt
+  echo "$LOGDATA == touch list_files_in_TO.txt">> $LOGFILE
+  echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" >> $LOGFILE
+  
+  echo "List files in TO path: $(pwd)/list_files_in_FROM.txt">> $Backup_FileName
+  echo "List files in FROM path: $(pwd)/list_files_in_TO.txt">> $Backup_FileName
+  echo "LOGFILE create at: $(pwd)/$LOGFILE">> $Backup_FileName
+  echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" >> $Backup_FileName
+  echo "Backup Setiings Done => $(date +%Y.%m.%d\ %Hh%Mm%Ss)" >> $Backup_FileName
+
+  echo "$(pwd)/$Backup_FileName creating ... Done!"
   echo "===================================================================="
   echo  
 }
@@ -89,25 +111,104 @@ func_Path_2Sync_Directories_Create() {
   #exit
 }
 
-funct_check_sync_path_from2to() {
-  echo "===================================================================="  
+func_check_sync_path_from2to() {
   Path_From_Check=$(cat $FILE | grep "$Line1_Backup_Settings" | sed 's/.*: //')
   #echo "$Path_From_Check"
   if [ ! -d "$Path_From_Check" ]; then
+    echo "===================================================================="  
     echo "!!! ----------------- ERROR ----------------- !!!"
     echo "!!! $Line1_Backup_Settings $Path_From_Check => missing, needs to be edited: $FILE"
+    echo "===================================================================="
+    echo "$LOGDATA == func_check_sync_path_from2to !!!ERROR!!! $Line1_Backup_Settings $Path_From_Check => missing, needs to be edited: $FILE" >> $for_log_file_info
+    exit
   fi
   
   Path_From_Check=$(cat $FILE | grep "$Line2_Backup_Settings" | sed 's/.*: //')
   #echo "$Path_From_Check"
   if [ ! -d "$Path_From_Check" ]; then
+    echo "===================================================================="
     echo "!!! ----------------- ERROR ----------------- !!!"
     echo "!!! $Line2_Backup_Settings $Path_From_Check => missing, needs to be edited: $FILE"
+    echo "===================================================================="
+    echo "$LOGDATA == func_check_sync_path_from2to !!!ERROR!!! $Line2_Backup_Settings $Path_From_Check => missing, needs to be edited: $FILE" >> $for_log_file_info
+    exit  
   fi
-  echo "===================================================================="  
   echo
 }
 
+
+func_copy_from2to() {
+  echo "----------func_copy_from2to------------------"
+ 
+  echo "$LOGDATA == func_copy_from2to RUN" >> $for_log_file_info
+
+  
+  path_FROM=$(cat $FILE | grep "$Line1_Backup_Settings" | sed 's/.*: //')
+  path_TO=$(cat $FILE | grep "$Line2_Backup_Settings" | sed 's/.*: //')
+  #echo $path_FROM
+  #echo $path_TO
+  
+  
+  list_files_FROM=$(cat $FILE | grep "List files in TO path" | sed 's/.*: //')
+  list_files_TO=$(cat $FILE | grep "List files in FROM path" | sed 's/.*: //')
+  #echo $list_files_FROM
+  #echo $list_files_TO
+  
+  echo "---------------------------"
+  echo "------ BEFORE Sync --------" 
+  echo "---------------------------" 
+  echo
+  
+  echo "...... Folder FROM consist from such files:"
+  # ls >> dir.txt
+  ls "$path_FROM" > $list_files_FROM
+  cat "$list_files_FROM"
+  echo
+  echo "...... In folder TO such files !!! WILL BE REMOVE !!!:"
+  ls "$path_TO" > $list_files_TO
+  cat "$list_files_TO"
+  echo 
+  
+  #diff "$list_files_FROM" "$list_files_TO"  
+  # diff - compare files line by line
+  #    -y, --side-by-side / output in two columns  
+  echo ":::::::::::::::::: DIFF between FROM & TO ::::::::::::::::::::::::"
+  echo "::::::::::::::::::       BEFORE Sync      ::::::::::::::::::::::::"
+  diff -y "$list_files_FROM" "$list_files_TO" > $Path_Config/changes.diff
+  cat $Path_Config/changes.diff 
+  echo
+  
+  # Remove files in 
+  rm -r "$path_TO/"
+  cp -r "$path_FROM/" "$path_TO/"
+  
+  
+  
+  echo "---------------------------"
+  echo "------- AFTER Sync --------"
+  echo "---------------------------"  
+  echo
+  
+  echo "...... Folder FROM consist from such files:"
+  ls "$path_FROM" > $list_files_FROM
+  cat "$list_files_FROM"
+  echo
+  echo "...... Folder TO consist from such files:"
+  ls "$path_TO" > $list_files_TO
+  cat "$list_files_TO"
+  echo 
+  
+  echo ":::::::::::::::::: DIFF between FROM & TO ::::::::::::::::::::::::" 
+  echo "::::::::::::::::::       AFTER Sync       ::::::::::::::::::::::::" 
+  diff -y "$list_files_FROM" "$list_files_TO" > $Path_Config/changes.diff
+  cat $Path_Config/changes.diff 
+  
+  echo "$LOGDATA == func_copy_from2to FINISH" >> $for_log_file_info
+  echo
+  exit
+  
+  #if         
+}
 
 # ==================================== ========= ====================================
 
@@ -121,23 +222,33 @@ if [ -w "$FILE" ]; then
   # -e file    Проверяет, существует ли файл.
   # -f file    Проверяет, существует ли файл, и является ли он файлом.
   # -w file    Проверяет, существует ли файл, и доступен ли он для записи.
-  echo "===================================================================="  
-  echo "$Backup_FileName exists, so Data Backup was configureted."
+  #echo "===================================================================="  
+  #echo "$Backup_FileName exists, so Data Backup was configureted."
   
-  echo "--------------------------------------------------------------------" 
+  #echo "--------------------------------------------------------------------" 
   
-funct_check_sync_path_from2to
+  func_check_sync_path_from2to
+  
+  
+  # Перенаправляем вывод в файл и показываем его на экране
+  #   Этот вариант подобен оператору > из предыдущего пункта, то имеется при записи в файл, 
+  #   все старые данные будут удалены. Если вам нужно дописать в файл, в конструкцию необходимо 
+  #   добавить параметр -a:
+  #          команда | tee -a /путь/к/файлу
+  
+  # Всю информацию с экрана о копировании файлов поместить в лог файл
+  func_copy_from2to | tee -a $for_log_file_info
+
   
   echo "!!!!!!!!!!!!!!! FINISh !!!!!!!!!!!!!!!!!!!!!!!!!"  
   echo "===================================================================="
 else 
 
-  
   func_Path_2Sync_Directories_Create
-  #cd~
-  
-  func_Backup_FileName_Create
-  
+  func_Backup_and_LOG_File_Create
+  echo "!!!!!!!!!!!!!!! RUN Script AGAIN for Sync !!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "===================================================================="
+  echo
 fi
 echo
 
