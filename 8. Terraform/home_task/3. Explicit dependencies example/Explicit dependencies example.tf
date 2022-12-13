1b204798e217ee0faa551882fb672b55f71e18ab
Ivan2navI <94798058+Ivan2navI@users.noreply.github.com>
2022-12-14 00:35:32 +0200

    ingress Security_Group_4Explicit

--- 8. Terraform/home_task/3. Explicit dependencies example/Explicit dependencies example.tf
+++ 8. Terraform/home_task/3. Explicit dependencies example/Explicit dependencies example.tf
@@ -34,9 +34,9 @@ resource "aws_security_group" "Security_Group_4Explicit" {
   #vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "ingress Security_Group_4Explicit"
-    from_port        = 443, 80, 22
-    to_port          = 443, 80, 22
+    description      = "Port 80 ingress Security_Group_4Explicit"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]   
@@ -51,6 +51,26 @@ resource "aws_security_group" "Security_Group_4Explicit" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  ingress {
+    description      = "Port 443 ingress Security_Group_4Explicit"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]   
+    #cidr_blocks      = [aws_vpc.main.cidr_block]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   tags = {
     Name = "Security_Group_4Explicit"
