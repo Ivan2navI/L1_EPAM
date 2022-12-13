ce6c8b1bf4ebca59a2a54750ef51fbf96c65c38e
Ivan2navI <94798058+Ivan2navI@users.noreply.github.com>
2022-12-13 21:51:58 +0200

    Create_aws_ec2_count.tf

---
+++ 8. Terraform/home_task/2. Use Count for create and destroy EC2/Create_aws_ec2_count.tf
@@ -0,0 +1,23 @@
+
+# Select cloud provider: AWS
+provider "aws" {
+    region = "eu-west-3"
+}
+
+# Amazon Linux 2 Kernel 5.10 AMI 2.0.20221103.3 x86_64 HVM gp2: 
+# ami-0f15e0a4c8d3ee5fe
+# -------------------------------------------------------------
+# Virtual server type (instance type)
+# t2.micro
+
+resource "aws_instance" "Count_Amazone_Linux" {
+    count         = 2                           # to add 2 (or more) identical EC2's 
+    ami           = "ami-0f15e0a4c8d3ee5fe"
+    instance_type = "t2.micro"                   
+
+    tags = {
+        Name    = "Amazon Linux (Count)"
+        Owner   = "DevOps Student"
+        Project = "Terraform (L1 EPAM)"
+    }
+}
