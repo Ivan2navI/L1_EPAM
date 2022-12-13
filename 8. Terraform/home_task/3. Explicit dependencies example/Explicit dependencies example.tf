d268642dbdbbdfcaa1e54f634cfd1c66cfd82c7a
Ivan2navI <94798058+Ivan2navI@users.noreply.github.com>
2022-12-14 00:50:31 +0200

    Add 2nd dependency. Creation DB before Web Server

--- 8. Terraform/home_task/3. Explicit dependencies example/Explicit dependencies example.tf
+++ 8. Terraform/home_task/3. Explicit dependencies example/Explicit dependencies example.tf
@@ -18,12 +18,17 @@ resource "aws_instance" "Web_Server_for_Explicit" {
 
     vpc_security_group_ids = [ "aws_security_group.Security_Group_4Explicit.id" ]           #1st dependency from aws_security_group
 
+    depends_on = [
+      aws_instance.Data_Base_for_Explicit                                                   # Add 2nd dependency => Creation DB before Web Server                                                
+    ]
+
     tags = {
         Name    = "Web Server (4Explicit)"
         Owner   = "DevOps Student"
         Project = "Explicit dependencies example (L1 EPAM)"
     }
 }
+
 
 resource "aws_instance" "Data_Base_for_Explicit" {
     ami           = "ami-0f15e0a4c8d3ee5fe"
