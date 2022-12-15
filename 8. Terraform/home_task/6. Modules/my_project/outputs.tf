a157b3a992ff4e631dc3cf23b5aadb8222212b4d
Ivan2navI <94798058+Ivan2navI@users.noreply.github.com>
2022-12-16 00:14:39 +0200

    Output_modif

--- 8. Terraform/home_task/6. Modules/my_project/outputs.tf
+++ 8. Terraform/home_task/6. Modules/my_project/outputs.tf
@@ -50,7 +50,7 @@ output "public_subnets_cidr_blocks" {
 
 output "database_subnets_cidr_blocks" {
   description = "List of cidr_blocks of database subnets"
-  value       = module.vpc.public_subnets_cidr_blocks
+  value       = module.vpc.database_subnets_cidr_blocks
 }
 
 output "vpc_public_subnets" {
