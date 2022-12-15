54202f4167a6a284e0aa11115ea4fca62a7dc481
Ivan2navI <94798058+Ivan2navI@users.noreply.github.com>
2022-12-16 00:13:45 +0200

    Output_modif

--- 8. Terraform/home_task/6. Modules/my_project/outputs.tf
+++ 8. Terraform/home_task/6. Modules/my_project/outputs.tf
@@ -27,7 +27,34 @@ output "ec2_instance_private_dns" {
 }
 
 
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "vpc_main_route_table_id" {
+  description = "The ID of the main route table associated with this VPC"
+  value       = module.vpc.vpc_main_route_table_id
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
+
+output "database_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of database subnets"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
+
 output "vpc_public_subnets" {
   description = "IDs of the VPC's public subnets"
   value       = module.vpc.public_subnets
 }
+
